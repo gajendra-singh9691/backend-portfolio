8d329a4bd597d580b97a1758bd08aef1864ecc19
Gajendra Singh <gajendrasingh12072003@gmail.com>
2025-10-16 03:19:06 +0530

    change in vercel.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv"
 import { connectDB } from "./DB/index.js";
 import app from "./app.js";
-dotenv.config({path : '../.env'});
+dotenv.config({path : './.env'});
 const port = process.env.PORT;
 connectDB()
 
